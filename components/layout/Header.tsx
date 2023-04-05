0411edd40ef7c4ec4c553a98d046b755799f3511
Mallang-Mallang <gudwns1458@naver.com>
2023-04-05 16:22:34 +0900

    [ Web ] Feat Header Location Css

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,13 +1,49 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import {
+  BellIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ArrowUpTrayIcon,
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
-const Header = () => {
-  return (
+interface HaederProps {
+  path: string;
+}
+
+const Header = ({ path }: HaederProps) => {
+  return path === '/location' ? (
+    <div className="flex-col w-[500px] h-[300px] px-5 justify-between items-center fixed top-0 left-0 right-0 ml-auto mr-auto bg-white z-20 border border-blue-500">
+      <div className="w-full h-[92px] flex justify-between items-center">
+        <ChevronLeftIcon className="w-[30px] h-[30px] cursor-pointer hover:text-blue-700" />
+        <ArrowUpTrayIcon className="w-[30px] h-[30px] cursor-pointer hover:text-blue-700" />
+      </div>
+
+      <div className="w-full h-[80px] flex justify-between items-center">
+        <ChevronLeftIcon className="w-[50px] h-[50px] cursor-pointer hover:text-blue-700" />
+        <h1 className="text-[32px] font-bold">대림대학교</h1>
+        <ChevronRightIcon className="w-[50px] h-[50px] cursor-pointer hover:text-blue-700" />
+      </div>
+
+      <div className="w-full h-[128px] flex justify-around items-center">
+        <div className="w-[100px] h-[50px] flex justify-center items-center text-white font-bold text-[18px] bg-blue-500 rounded-full">
+          카페
+        </div>
+        <div className="w-[100px] h-[50px] flex justify-center items-center text-white font-bold text-[18px] bg-blue-500 rounded-full">
+          영화
+        </div>
+        <div className="w-[100px] h-[50px] flex justify-center items-center text-white font-bold text-[18px] bg-blue-500 rounded-full">
+          전시
+        </div>
+        <div className="w-[100px] h-[50px] flex justify-center items-center text-white font-bold text-[18px] bg-blue-500 rounded-full">
+          공연
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="flex w-[500px] h-[60px] px-5 justify-between items-center fixed top-0 left-0 right-0 ml-auto mr-auto bg-white z-20 border border-blue-500">
-      <Link href="/">
-        <h1 className="my-auto hover:cursor-pointer">다모임</h1>
-      </Link>
-      <MagnifyingGlassIcon className="w-[30px] h-[30px] cursor-pointer hover:text-orange-500" />
+      <MagnifyingGlassIcon className="w-[30px] h-[30px] cursor-pointer hover:text-blue-700" />
+      <BellIcon className="w-[30px] h-[30px] cursor-pointer hover:text-blue-700" />
     </div>
   );
 };
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,12 +1,14 @@
 import { PropsWithChildren } from 'react';
 import Header from './Header';
 import Tabbar from './Tabbar';
+import { useRouter } from 'next/router';
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const router = useRouter();
   return (
     <div className="flex justify-center h-screen bg-zinc-50 overflow-auto">
       <div className="w-[500px] h-full mt-[60px]">
-        <Header />
+        <Header path={router.pathname} />
         {children}
         <Tabbar />
       </div>
