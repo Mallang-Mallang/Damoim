52f9866b6f15ccb6776626da4131fcaf9292e2c3
UBamtol <yij0325@gmail.com>
2023-04-05 14:23:47 +0900

    [ Dev ] Add MainPage

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -5,7 +5,7 @@ import Tabbar from './Tabbar';
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex justify-center h-screen bg-zinc-50 overflow-auto">
-      <div className="w-[500px] mt-[60px] mb-[65px]  border border-red-500">
+      <div className="w-[500px] h-full mt-[60px]">
         <Header />
         {children}
         <Tabbar />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import Map from '@/components/common/Map';
 import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -10,7 +12,53 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Map latitude={37.403256570432745} longitude={126.9306839711054} />
+
+      <div className="px-5 h-[910px] bg-white">
+        <div className="flex justify-between items-center mb-[105px]">
+          <h1 className="text-[30px] font-semibold">전체 모임</h1>
+          <div className="flex border-2 border-[#43ABE5] text-[#43ABE5] font-semibold w-[120px] h-[40px] rounded-full justify-center items-center">
+            일정 추가
+          </div>
+        </div>
+        <div className="w-full flex flex-wrap justify-between gap-y-[10px]">
+          <div className="w-[225px] h-[220px] bg-[#EAF7FF] rounded-[40px] p-5">
+            <div className="py-[10px] font-semibold text-xl">
+              스터디 그룹 모임
+            </div>
+            <div className="test-xl">그날 뵙겠습니다.</div>
+          </div>
+          <div className="w-[225px] h-[220px] bg-[#FFF0EA] rounded-[40px] p-5">
+            <div className="py-[10px] font-semibold text-xl">
+              스터디 그룹 모임
+            </div>
+            <div className="test-xl">그날 뵙겠습니다.</div>
+          </div>
+          <div className="w-[225px] h-[220px] bg-[#FFF0EA] rounded-[40px] p-5">
+            <div className="py-[10px] font-semibold text-xl">
+              스터디 그룹 모임
+            </div>
+            <div className="test-xl">그날 뵙겠습니다.</div>
+          </div>
+          <div className="w-[225px] h-[220px] bg-[#EAF7FF] rounded-[40px] p-5">
+            <div className="py-[10px] font-semibold text-xl">
+              스터디 그룹 모임
+            </div>
+            <div className="test-xl">그날 뵙겠습니다.</div>
+          </div>
+          <div className="w-[225px] h-[220px] bg-[#EAF7FF] rounded-[40px] p-5">
+            <div className="py-[10px] font-semibold text-xl">
+              스터디 그룹 모임
+            </div>
+            <div className="test-xl">그날 뵙겠습니다.</div>
+          </div>
+          <div className="w-[225px] h-[220px] bg-[#FFF0EA] rounded-[40px] p-5">
+            <div className="py-[10px] font-semibold text-xl">
+              스터디 그룹 모임
+            </div>
+            <div className="test-xl">그날 뵙겠습니다.</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
