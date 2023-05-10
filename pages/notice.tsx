0e39c35f7fb66df34d00dd9b669a60b64e407292
Mallang-Mallang <gudwns1458@naver.com>
2023-05-10 14:22:44 +0900

    [ Web ] Add Notice pages

---
+++ pages/notice.tsx
@@ -0,0 +1,20 @@
+import Notification from '@/components/Notification';
+import React from 'react';
+
+function notice() {
+  return (
+    <>
+      <Notification />
+      <Notification />
+      <Notification />
+      <Notification />
+      <Notification />
+      <Notification />
+      <Notification />
+      <Notification />
+      <Notification />
+    </>
+  );
+}
+
+export default notice;
