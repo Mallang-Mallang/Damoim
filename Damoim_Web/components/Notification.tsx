44be051e1dc353b9872be7bc4c8301332d317baf
Mallang-Mallang <gudwns1458@naver.com>
2023-06-20 16:53:51 +0900

    Revert "[ Web ] Feat Remove test.tsx"
    This reverts commit 642f9b814f728e8a7dd968666c25447207263e89.

--- Damoim_Web/components/Notification.tsx
+++ Damoim_Web/components/Notification.tsx
@@ -1,21 +1,14 @@
-import Image from 'next/image';
-import { useSession } from 'next-auth/react';
+import React from 'react';
 
 function Notification() {
-  const { data: session } = useSession();
-  console.log(session);
   return (
-    <div className="flex items-center w-full h-[100px] p-3 ">
-      <Image
-        src={session?.user?.image!}
-        width={50}
-        height={50}
-        className="border rounded-full m-3"
-        alt="profile"
-      />
+    <div className="flex items-center w-full h-[100px] p-2 ">
+      <div className="flex justify-center items-start pt-3 w-[100px] h-full">
+        <div className="w-[50px] h-[50px] bg-slate-500 rounded-full"></div>
+      </div>
 
       <div className="flex flex-col justify-around w-full h-full">
-        {session?.user?.name}님이 모임을 추가했습니다.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, illo amet
         <br />
         <span className="text-xs text-gray-500">35분 전</span>
       </div>
